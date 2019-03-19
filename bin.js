8649db634229057faba3e8055677a203a9fc8f9b
Gewondjan <jedimasterryan@byui.edu>
2019-03-19 14:02:18 -0600

    working on stubbing

---
+++ bin.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/************************************************************************* 
+ * Libraries / Requires / Constants
+ *************************************************************************/
+const main = require('./main.js');
+const asyncLib = require('async');
+/************************************************************************* 
+ * Input Function
+ * If it is complicated, consider abstracting it to a seperate file.
+ *************************************************************************/
+function getInput(data, callback) {
+    var input;
+    // How to get input, eg. from file, commandline, inquierer, etc.
+    return callback(null, input);
+}
+
+/************************************************************************* 
+ * Output Function
+ * If it is complicated, consider abstracting it to a seperate file.
+ *************************************************************************/
+function getOutput(data, callback) {
+    // How to output data, eg. to csv, to json, to console, etc.
+    return callback(null, data);
+}
+
+
+//Start here
+asyncLib.waterfall([
+    getInput,
+    main,
+    getOutput,
+], console.log);
